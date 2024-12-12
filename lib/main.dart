e6ced5486d014b6bdb1f8d46e56d2dd2adea0bd5
Christian yuda Pratama <53987991+christyuda@users.noreply.github.com>
2024-12-12 10:51:04 +0700

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -37,7 +37,7 @@ class MyApp extends StatelessWidget {
           debugShowCheckedModeBanner: false,
           onGenerateRoute: (settings) =>
               router.generateRoute(settings, currentDomain),
-          initialRoute: '/',
+          initialRoute: '/backoffice',
           builder: EasyLoading.init(),
         ),
       ),
